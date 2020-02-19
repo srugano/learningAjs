11cb0399da57a1e396a32945888bd2fbbe19e4ac
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2020-02-19 14:50:06 +0100

    Extending the User Model for Authentication

---
+++ flapper-news/models/Users.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  hash: String,
+  salt: String
+});
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  /* The pbkdf2Sync() function takes four parameters: password, salt, iterations, and key length.
+   We'll need to make sure the iterations and key length in our setPassword()
+    method match the ones in our validPassword() method */
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'Secret to be changed as environement variable');
+};
+
+mongoose.model('User', UserSchema);
